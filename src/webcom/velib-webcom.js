27beedbf539dbb1fa5c2340326fdd815789bd11f
vboulaye <vboulaye@gmail.com>
2020-01-13 02:55:17 +0100

    make it work wiathout webcom credential

--- src/webcom/velib-webcom.js
+++ src/webcom/velib-webcom.js
@@ -3,19 +3,19 @@ const Webcom = require('./webcom-custom.js')
 const WebComSecret = require('./webcom-secret.js')
 
 function VelibWebcom () {
-  try {
-    const ref = new Webcom('https://io.datasync.orange.com/base/my-velib')
-    return {
-      // login and return the ref to the base ref
-      auth: () => ref.auth(WebComSecret).then(() => ref),
-
+  return {
+    // login and return the ref to the base ref
+    auth: () => {
+      try {
+        const ref = new Webcom('https://io.datasync.orange.com/base/my-velib')
+        return ref
+          .auth(WebComSecret)
+          .then(() => ref)
+      } catch (e) {
+        console.warn('unable to connect to webcom, working without webcom: ' + e)
+        return Promise.resolve(null)
+      }
     }
-  } catch (e) {
-    console.error('unable to connect to webcom ' + e)
-    return {
-      auth: () => null,
-    }
-
   }
 } // end constructor
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,79 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
+const fs = require('fs')
 
-const mode = process.env.NODE_ENV || 'development';
-const prod = mode === 'production';
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
 
+const plugins = []
+plugins.push(
+  new CopyPlugin([
+    { from: 'public', to: '.' },
+  ]))
+
+if (!fs.existsSync('src/webcom/webcom-secret.js')) {
+  plugins.push(
+    new webpack.NormalModuleReplacementPlugin(/(.*)webcom-secret\.js/, function (resource) {
+      resource.request = resource.request.replace(/webcom-secret\.js/, `webcom-secret.example.js`)
+    })
+  );
+}
 module.exports = {
-	entry: {
-		bundle: ['./src/main.js']
-	},
-	resolve: {
-		extensions: ['.mjs', '.js', '.svelte'],
-		alias:{
-			//faye: 'faye/browser/faye-browser' , // alias needed because webpack use the node version
-		}
-	},
-	output: {
-		path: __dirname + '/dist',
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	plugins: [
-		new CopyPlugin([
-			{ from: 'public', to: '.' },
-		]),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.svelte$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						emitCss: true,
-						hotReload: true
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					/**
-					 * MiniCssExtractPlugin doesn't support HMR.
-					 * For developing, use 'style-loader' instead.
-					 * */
-					//prod ? MiniCssExtractPlugin.loader : 'style-loader',
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-				},
-			},
-		]
-	},
-	mode,
-	// plugins: [
-	// 	new MiniCssExtractPlugin({
-	// 		filename: '[name].css'
-	// 	})
-	// ],
-	devtool: prod ? false: 'source-map'
-};
+  entry: {
+    bundle: ['./src/main.js']
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.svelte'],
+    alias: {
+      //faye: 'faye/browser/faye-browser' , // alias needed because webpack use the node version
+    }
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js'
+  },
+  plugins: plugins,
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            emitCss: true,
+            hotReload: true
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          /**
+           * MiniCssExtractPlugin doesn't support HMR.
+           * For developing, use 'style-loader' instead.
+           * */
+          //prod ? MiniCssExtractPlugin.loader : 'style-loader',
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ]
+  },
+  mode,
+  // plugins: [
+  // 	new MiniCssExtractPlugin({
+  // 		filename: '[name].css'
+  // 	})
+  // ],
+  devtool: prod ? false : 'source-map'
+}
